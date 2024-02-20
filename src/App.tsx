3e010adb77c694f21c0f3e5fe53f5fd26ad519f5
inathan44 <inathan44@yahoo.com>
2024-02-19 22:22:14 -0500

    styling: place components in place

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import './App.css';
+import "./App.css";
+import ColHeaders from "./grid/ColHeaders";
 
-import Row from './grid/Row';
+import Row from "./grid/Row";
+import RowHeader from "./grid/RowHeaders";
+import Ribbon from "@/ribbon/Ribbon";
+import { useGridStore } from "./store";
 
 function App() {
+  const grid = useGridStore((state) => state.grid);
+
   return (
     <div>
-      <Row rows={5} />
+      <Ribbon />
+      <div className="flex">
+        <div className="h-4 w-8 text-transparent">Placeholder text</div>
+        <ColHeaders />
+      </div>
+      <div className="flex border-collapse">
+        <RowHeader />
+
+        <div className="">
+          {grid.map((row, i) => (
+            <Row key={i} row={row} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
